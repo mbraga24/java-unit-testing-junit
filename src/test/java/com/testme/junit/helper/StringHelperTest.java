81c2c710d181b4ea194ed013b1bd653eccee0f42
mrdbrg <rdg.brg@gmail.com>
2022-05-07 20:07:29 -0400

    avoid duplication

--- src/test/java/com/testme/junit/helper/StringHelperTest.java
+++ src/test/java/com/testme/junit/helper/StringHelperTest.java
@@ -8,13 +8,13 @@ import com.testme.junit.helper.StringHelper;
 
 public class StringHelperTest extends StringHelper {
 
+	StringHelper helper = new StringHelper();
+	
 	/*
 	 * Passing AABC => BC test
 	 */
 	@Test
 	public void testTruncateAInFirst2Positions_AABC_Success() {
-		
-		StringHelper helper = new StringHelper();
 		
 		String actual = helper.truncateAInFirst2Positions("AABC");
 		String expected = "BC";
@@ -27,8 +27,6 @@ public class StringHelperTest extends StringHelper {
 	 */
 	@Test
 	public void testTruncateAInFirst2Positions_ABC_Success() {
-		
-		StringHelper helper = new StringHelper();
 		
 		String actual = helper.truncateAInFirst2Positions("ABC");
 		String expected = "BC";
